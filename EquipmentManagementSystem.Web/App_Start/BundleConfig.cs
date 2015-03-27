4ffeea445a0860530acdf70f4d179e388c80d38c
phatnguyen81@gmail.com <phatnguyen81@gmail.com>
2015-03-27 16:54:39 +0700

    Update

--- EquipmentManagementSystem.Web/App_Start/BundleConfig.cs
+++ EquipmentManagementSystem.Web/App_Start/BundleConfig.cs
@@ -20,12 +20,25 @@ namespace EquipmentManagementSystem.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js",
+                "~/Scripts/sb-admin-2.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/sb-admin-2.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
+                "~/Scripts/plugins/metisMenu/metisMenu.min.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/plugins").Include(
+                "~/Content/plugins/metisMenu/metisMenu.min.css",
+                "~/Content/plugins/morris.css",
+                "~/Content/plugins/timeline.css"));
+
+            bundles.Add(new StyleBundle("~/fonts").Include(
+                "~/fonts/font-awesome/css/font-awesome.min.css"));
         }
     }
 }
