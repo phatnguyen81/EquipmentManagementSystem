f43df21206e00eaefef985d6eba885f854b53a99
phatnguyen81@gmail.com <phatnguyen81@gmail.com>
2015-03-27 13:41:07 +0700

    Create New Solution

---
+++ EquipmentManagementSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EquipmentManagementSystem.Web.Startup))]
+namespace EquipmentManagementSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
